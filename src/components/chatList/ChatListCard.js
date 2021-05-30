2eb6066af160b4f5ab92f63e2c426b44194127f1
mehedi23 <satusatusatu0@gmail.com>
2021-05-30 17:39:08 +0600

    user list animation completed without remove user

--- src/components/chatList/ChatListCard.js
+++ src/components/chatList/ChatListCard.js
@@ -1,15 +1,22 @@
-import React , {useState , useEffect} from 'react';
+import React , {useState , useEffect , useRef} from 'react';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import styled from '../../static/css/chatList.module.css';
 import { Tween , Timeline } from 'react-gsap';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
 
 const ChatListCard = (props) => {
+
+    let counter = 0;
 
     const [ days , setDays ] = useState(0);
     const [ hours , setHours ] = useState(0);
     const [ minutes , setMinutes ] = useState(0);
+    const [ ms , setMs] = useState(counter);
+    const [ removeBtn , setRemoveBtn ] = useState(false)
 
-    const { lastTime } = props
+    const { lastTime } = props;
+    let timerinterval = useRef(null);
 
 
     useEffect(()=>{
@@ -31,7 +38,52 @@ const ChatListCard = (props) => {
 
     },[lastTime])
 
+    
+    const timer = (start) => {
+        if (start === true && counter >= 1) {
+                timerinterval.current = setInterval(() => {
+                setMs(counter); 
+                counter += 1;
+            }, [1000]);
+        }
+    };
+
+    useEffect(()=>{
+        if(ms > 0){
+
+            setTimeout(()=>{
+                setRemoveBtn(true)
+            } , 1000)
+            
+        }
+    },[ms])
+
+    const pressingDown = (e) => {
+        counter = 1;
+        timer(true);
+    };
+
+    const notPressingDown = (e) => {
+        timer(false);
+        clearInterval(timerinterval.current);
+    };
+
+    const handleClick = () => {
+        setMs(0);
+        counter = 0;
+        setRemoveBtn(false);
+    };
+
+    const handleClick_two = () =>{
+        if(removeBtn){
+            setMs(0);
+            counter = 0;
+            setRemoveBtn(false);
+        }
+    }
+
     return (
+        <ClickAwayListener onClickAway={handleClick}>
             <Tween 
                 from={{
                     x:"-60px",
@@ -40,41 +92,71 @@ const ChatListCard = (props) => {
                 duration={1}
                 delay={props.index/3}
             >
+                
                 <CardActionArea 
+                    color='inherit'
+                    component='div'
+                    onMouseDown={pressingDown}
+                    onMouseLeave={notPressingDown}
+                    onMouseUp={notPressingDown}
+                    onTouchStart={pressingDown}
+                    onTouchEnd={notPressingDown}
                     className={styled.theCard} 
                 >
                     
-                    <div className={styled.userImg}>
-                        <Timeline 
-                            target={ <img src={props.img} alt="user-img"/> }>
-                            <Tween from={{x:"60px"}}  delay={props.index/3}/>
-                        </Timeline>
-                    </div>
+                        <div className={styled.userImg}>
+                            <Timeline 
+                                target={ <img src={props.img} alt="user-img"/> }>
+                                <Tween from={{x:"60px"}}  delay={props.index/3}/>
+                            </Timeline>
+                        </div>
 
-                    <div className={styled.userName}>
-                        <h1> {props.name} </h1>
-                        <p> {props.text} </p>
-                    </div>
+                        <div className={styled.userName}>
+                            <h1> {props.name} </h1>
+                            <p> {props.text} </p>
+                        </div>
 
-                    <div className={styled.userUnread}>
-                        { props.unread > 0 && 
-                            <p className={styled.userUnreadNum}> {props.unread} </p> 
-                        } 
+                        <div className={styled.userUnread}>
+                            { props.unread > 0 && 
+                                <p className={styled.userUnreadNum}> {props.unread} </p> 
+                            } 
 
-                        <p className={styled.userLastSeen}> 
-                            {
-                                days && days > 0 ?
-                                    days + 'd' : 
-                                    hours && hours > 0 ?
-                                        hours + 'h' :
-                                        minutes && minutes > 0 ?
-                                        minutes + 'm' : 'now'
-                            }
-                        </p>
+                            <p className={styled.userLastSeen}> 
+                                {
+                                    days && days > 0 ?
+                                        days + 'd' : 
+                                        hours && hours > 0 ?
+                                            hours + 'h' :
+                                            minutes && minutes > 0 ?
+                                            minutes + 'm' : 'now'
+                                }
+                            </p>
+                        </div>
 
-                    </div>
+                        {
+                            ms > 0 && 
+                                <div className={styled.user_remove_btn}>
+                                    <ClickAwayListener onClickAway={handleClick_two}>
+                                    <Timeline 
+                                        target={ <button>
+                                                    <Tween from={{x:"10px"}} duration={.5} ease="elastic.out(1, 0.3)"/>
+                                                    
+                                                    <Timeline target={<DeleteForeverTwoToneIcon/>}>
+                                                        <Tween to={{x:"0px"}} from={{x:"-10px"}} duration={.5} ease="elastic.out(1, 0.3)" />
+                                                        <Tween to={{x:"0px"}} duration={.25}  delay={.3}/>
+                                                    </Timeline>
+                                                </button> }>
+                                        
+                                    </Timeline>
+                                    </ClickAwayListener>
+                                </div>
+                        }
+                    
                 </CardActionArea>
+                
             </Tween>
+        </ClickAwayListener>
+        
     )
 }
 
